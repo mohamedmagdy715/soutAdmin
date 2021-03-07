22944a126838a0bfbe3f2df9321cd8df96c9ce0a
mohamedmagdy715 <mohamedmagdy715@gmail.com>
2021-03-08 01:51:15 +0200

    github pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyB34ZDbD7bLBLklSgRbyaEqdOo-ZB1V8dw",
+    authDomain: "sout-2d0f6.firebaseapp.com",
+    projectId: "sout-2d0f6",
+    storageBucket: "sout-2d0f6.appspot.com",
+    messagingSenderId: "706376997886",
+    appId: "1:706376997886:web:36a3101d876f715a217eaf",
+    measurementId: "G-5R8JHQGN68"
+  }
 };
